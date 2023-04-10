3ffa5363edc800e3aac07ab0e25788a907905482
uniqname <uniqname@umich.edu>
2023-04-10 15:43:18 -0400

    core functions

--- HW8.py
+++ HW8.py
@@ -1,6 +1,6 @@
-# Your name: 
-# Your student id:
-# Your email:
+# Your name: Debangi Mohanta
+# Your student id: 65812361
+# Your email: debangim@umich.edu
 # List who you have worked with on this homework:
 
 import matplotlib.pyplot as plt
@@ -15,7 +15,34 @@ def load_rest_data(db):
     and each inner key is a dictionary, where the key:value pairs should be the category, 
     building, and rating for the restaurant.
     """
-    pass
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db)
+    cur = conn.cursor()
+
+    cur.execute('SELECT name, rating, category_id, building_id FROM restaurants')
+    bigtable = cur.fetchall()
+    cur.execute('SELECT category, id FROM categories')
+    categorytable= cur.fetchall()
+    cur.execute('SELECT building, id FROM buildings')
+    buildingstable = cur.fetchall()
+    conn.commit()
+
+    restaurant_dictionary = {}
+    for the_tuple in bigtable:
+        dict = {}
+        for cat in categorytable:
+            if cat[1] == the_tuple[2]:
+                dict['category'] = cat[0]
+                break
+        for building in buildingstable:
+            if building[1] == the_tuple[3]:
+                dict['building'] = building[0]
+                break
+        dict['rating'] = the_tuple[1]
+        restaurant_name = the_tuple[0]
+        restaurant_dictionary[restaurant_name] = dict
+
+    return restaurant_dictionary
 
 def plot_rest_categories(db):
     """
@@ -23,7 +50,38 @@ def plot_rest_categories(db):
     restaurant categories and the values should be the number of restaurants in each category. The function should
     also create a bar chart with restaurant categories and the count of number of restaurants in each category.
     """
-    pass
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db)
+    cur = conn.cursor()
+    cur.execute('SELECT categories.category, categories.id FROM categories')
+    categories = cur.fetchall()
+    conn.commit()
+
+    cur.execute('SELECT category_id, COUNT(*) as COUNT FROM restaurants GROUP BY CATEGORY_ID')
+    number = cur.fetchall()
+    conn.commit()
+
+    restaurant_categories_dictionary = {}
+    counter = 0
+    for category in categories:
+        restaurant_categories_dictionary[category[0]] = number[counter][1]
+        counter += 1
+
+    sorted_restaurant_categories_dictionary= dict(sorted(restaurant_categories_dictionary.items()))
+    descending_restaurant_categories_dictionary = dict(sorted(restaurant_categories_dictionary.items(),key=lambda item: item[1],reverse=False))
+    
+    #bar chart
+    restaurants = list(descending_restaurant_categories_dictionary.keys())
+    count = list(descending_restaurant_categories_dictionary.values())
+
+    plt.barh(restaurants, count, color=['darkblue'])
+    plt.title('Types of Restaurant on South University Ave')
+    plt.xlabel('Number of Restaurants')
+    plt.ylabel('Restaurant Categories')
+    plt.tight_layout()
+    plt.show()
+
+    return sorted_restaurant_categories_dictionary
 
 def find_rest_in_building(building_num, db):
     '''
@@ -31,7 +89,16 @@ def find_rest_in_building(building_num, db):
     restaurant names. You need to find all the restaurant names which are in the specific building. The restaurants 
     should be sorted by their rating from highest to lowest.
     '''
-    pass
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db)
+    cur = conn.cursor()
+
+    cur.execute('''SELECT r.name FROM restaurants r JOIN buildings b ON r.building_id = b.id WHERE b.building = ? ORDER BY r.rating DESC; ''', (building_num,))
+    restaurant_names = [row[0] for row in cur.fetchall()]
+
+    conn.close()
+
+    return restaurant_names
 
 #EXTRA CREDIT
 def get_highest_rating(db): #Do this through DB as well
